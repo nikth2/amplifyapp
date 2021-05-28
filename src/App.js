f85f20ddea964401fe8ea16f15e3f58c9db0d03a
Nikos Theodoropoulos <ntheodor@cosmote.gr>
2021-05-28 18:38:58 +0300

    added front-end code

--- src/App.js
+++ src/App.js
@@ -1,55 +1,66 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import React from 'react';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { API } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
-class App extends React.Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = { username: '' };
-  }
-  
-  myChangeHandler = (event) => {
-    this.setState({username: event.target.value});
+const initialFormState = { name: '', description: '' }
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    setNotes(apiData.data.listNotes.items);
   }
 
-  mySubmitHandler = (event) => {
-    event.preventDefault();
-    //alert("You are submitting " + this.state.username);
-    fetch("http://localhost:3000").then(result => alert(result.status));
+  async function createNote() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload!
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-          <form onSubmit={this.mySubmitHandler}>
-            <h1>Hello {this.state.username}</h1>
-            <p>Enter your name:</p>
-            <input
-              type="text" onChange={this.myChangeHandler}
-            />
-          </form>
-        </header>
-        <AmplifySignOut />
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  }
+
+  return (
+    <div className="App">
+      <h1>My Notes App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Note description"
+        value={formData.description}
+      />
+      <button onClick={createNote}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+        {
+          notes.map(note => (
+            <div key={note.id || note.name}>
+              <h2>{note.name}</h2>
+              <p>{note.description}</p>
+              <button onClick={() => deleteNote(note)}>Delete note</button>
+            </div>
+          ))
+        }
       </div>
-    );
-  }
+      <AmplifySignOut />
+    </div>
+  );
 }
-
-
 
 export default withAuthenticator(App);
