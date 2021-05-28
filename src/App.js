676a4c21b3f0f740d7d7c1024ae654455add6c86
Nikos Theodoropoulos <ntheodor@cosmote.gr>
2021-05-28 17:56:40 +0300

    added auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { username: '' };
+  }
+  
+  myChangeHandler = (event) => {
+    this.setState({username: event.target.value});
+  }
+
+  mySubmitHandler = (event) => {
+    event.preventDefault();
+    //alert("You are submitting " + this.state.username);
+    fetch("http://localhost:3000").then(result => alert(result.status));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+          <form onSubmit={this.mySubmitHandler}>
+            <h1>Hello {this.state.username}</h1>
+            <p>Enter your name:</p>
+            <input
+              type="text" onChange={this.myChangeHandler}
+            />
+          </form>
+        </header>
+        <AmplifySignOut />
+      </div>
+    );
+  }
 }
 
-export default App;
+
+
+export default withAuthenticator(App);
